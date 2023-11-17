c9e6d4daa9cf80b9fe87bd37149f8ab302bd9f57
kevinnls <developer@kevinnlsamuel.com>
2023-11-17 08:15:27 +0530

    styles for aca

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig){
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 	eleventyConfig.addPassthroughCopy({
 		"./node_modules/@picocss/pico/css/pico.min.*": "./",
+		"./aca.css": "./",
 	}, {debug: true})
 	eleventyConfig.addPassthroughCopy("**/*.jpg");
 
